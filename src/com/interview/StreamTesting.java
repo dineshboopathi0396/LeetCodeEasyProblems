7af46d0bd5670ab20863dd3718da09de4e7f388e
Dinesh BoopathiKannan <86138162+dineshboopathi0396@users.noreply.github.com>
2024-05-09 23:51:05 +0530

    Streams

---
+++ src/com/interview/StreamTesting.java
@@ -0,0 +1,96 @@
+package com.interview;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamTesting {
+
+    public static void main(String[] args) {
+        List<Student> studentList = loadStudents();
+
+        //1- Find list of students whose first name starts with alphabet A
+        List<Student> startsWithA = studentList.stream().filter(s -> s.getFirstName().startsWith("A")).collect(Collectors.toList());
+        System.out.println(startsWithA);
+
+        //2- Group The Student By Department Names
+        Map<String, List<Student>> studentsByDepartment = studentList.stream().collect(Collectors.groupingBy(s -> s.getDepartmentName()));
+        System.out.println(studentsByDepartment);
+
+        //3- Find the total count of student using stream
+        long count = studentList.stream().count();
+        System.out.println(count);
+
+        //4- Find the max age of student
+        Optional<Student> maxAgeStudent = studentList.stream().max(Comparator.comparingInt(Student::getAge));
+        OptionalInt maxAge = studentList.stream().mapToInt(s -> s.getAge()).max();
+        System.out.println(maxAge.getAsInt());
+
+        //5- Find all departments names
+        List<String> departments = studentList.stream().map(s -> s.getDepartmentName()).distinct()
+                .collect(Collectors.toList());
+        System.out.println(departments);
+
+        //6- Find the count of student in each department
+        Map<String, Long> studentCount = studentList.stream().collect(Collectors.groupingBy(s -> s.getDepartmentName(), Collectors.counting()));
+        System.out.println(studentCount);
+
+        //7- Find the list of students whose age is less than 30
+        List<Student> studentsLessThan30 = studentList.stream().filter(s -> s.getAge() < 30).collect(Collectors.toList());
+        System.out.println(studentsLessThan30);
+
+        //8- Find the list of students whose rank is in between 50 and 100
+        List<Student> studentsByRank = studentList.stream().filter(s -> s.getRank() >= 50 && s.getRank() <= 100).collect(Collectors.toList());
+        System.out.println(studentsByRank);
+
+        //9- Find the average age of male and female students
+        Map<String, Double> avgAge = studentList.stream()
+                .collect(Collectors.groupingBy(s -> s.getGender(), Collectors.averagingDouble(Student::getAge)));
+        System.out.println(avgAge);
+
+        //10- Find the department who is having maximum number of students
+        Map.Entry<String, Long> maxDepartment = studentList.stream().collect(Collectors.groupingBy(Student::getDepartmentName, Collectors.counting()))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).get();
+        System.out.println(maxDepartment);
+
+        //11- Find the Students who stays in Delhi and sort them by their names
+        List<Student> sortedStudents = studentList.stream().filter(s -> s.getCity().equals("Delhi"))
+                .sorted(Comparator.comparing(Student::getFirstName))
+                .collect(Collectors.toList());
+        System.out.println(sortedStudents);
+
+        //12- Find the average rank in all departments
+        Map<String, Double> avgRankInDepartments = studentList.stream()
+                .collect(Collectors.groupingBy(Student::getDepartmentName, Collectors.averagingDouble(Student::getRank)));
+        System.out.println(avgRankInDepartments);
+
+        //13- Find the highest rank in each department
+        Map<String, Optional<Student>> HighestRank = studentList.stream()
+                .collect(Collectors.groupingBy(Student::getDepartmentName, Collectors.minBy(Comparator.comparing(Student::getRank))));
+        System.out.println(HighestRank);
+
+        //14- Find the list of students and sort them by their rank
+        List<Student> stuRankSorted = studentList.stream().sorted(Comparator.comparing(Student::getRank))
+                .collect(Collectors.toList());
+        System.out.println("List of students sorted by their rank  : " + stuRankSorted);
+
+        //15- Find the student who has second rank
+        Student student = studentList.stream().sorted(Comparator.comparing(Student::getRank))
+                .skip(1).findFirst().get();
+        System.out.println(student);
+
+    }
+
+    public static List<Student> loadStudents() {
+        return Arrays.asList(
+                new Student(1, "Rohit", "Mall", 30, "Male", "Mechanical Engineering", 2015, "Mumbai", 122),
+                new Student(2, "Pulkit", "Singh", 56, "Male", "Computer Engineering", 2018, "Delhi", 67),
+                new Student(3, "Ankit", "Patil", 25, "Female", "Mechanical Engineering", 2019, "Kerala", 164),
+                new Student(4, "Satish Ray", "Malaghan", 30, "Male", "Mechanical Engineering", 2014, "Kerala", 26),
+                new Student(5, "Roshan", "Mukd", 23, "Male", "Biotech Engineering", 2022, "Mumbai", 12),
+                new Student(6, "Chetan", "Star", 24, "Male", "Mechanical Engineering", 2023, "Karnataka", 90),
+                new Student(7, "Arun", "Vittal", 26, "Male", "Electronics Engineering", 2014, "Karnataka", 324),
+                new Student(8, "Nam", "Dev", 31, "Male", "Computer Engineering", 2014, "Karnataka", 433),
+                new Student(9, "Sonu", "Shankar", 27, "Female", "Computer Engineering", 2018, "Karnataka", 7),
+                new Student(10, "Shubham", "Pandey", 26, "Male", "Instrumentation Engineering", 2017, "Mumbai", 98));
+    }
+}
---
+++ src/com/interview/Student.java
@@ -0,0 +1,149 @@
+package com.interview;
+
+
+public class Student {
+
+    public Student(int id, String firstName, String lastName, int age, String gender, String departmentName, int joinedYear, String city, int rank) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.departmentName = departmentName;
+        this.joinedYear = joinedYear;
+        this.city = city;
+        this.rank = rank;
+    }
+
+    /**
+     * The id.
+     */
+    private int id;
+
+    /**
+     * The first name.
+     */
+    private String firstName;
+
+    /**
+     * The last name.
+     */
+    private String lastName;
+
+    /**
+     * The age.
+     */
+    private int age;
+
+    /**
+     * The gender.
+     */
+    private String gender;
+
+    /**
+     * The department name.
+     */
+    private String departmentName;
+
+    /**
+     * The joined year.
+     */
+    private int joinedYear;
+
+    /**
+     * The city.
+     */
+    private String city;
+
+    /**
+     * The rank.
+     */
+    private int rank;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getJoinedYear() {
+        return joinedYear;
+    }
+
+    public void setJoinedYear(int joinedYear) {
+        this.joinedYear = joinedYear;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", departmentName='" + departmentName + '\'' +
+                ", joinedYear=" + joinedYear +
+                ", city='" + city + '\'' +
+                ", rank=" + rank +
+                '}';
+    }
+}
